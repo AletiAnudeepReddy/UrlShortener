4045309fce020095972c2703bee0c8838154a71c
anudeep096 <aletianudeepreddy8@gmail.com>
2025-07-03 15:28:21 +0530

    header

--- app/page.js
+++ app/page.js
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import localFont from "next/font/local"
+import Link from "next/link";
+const  poppins= localFont({
+  src:"./Poppins-ExtraBold.ttf",
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <main>
-      I am main.
+    <main className="bg-purple-100">
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="flex flex-col items-center gap-4 justify-center">
+          <p className={`text-3xl font-bold ${poppins.className}`}>
+            The Best URL Shortener in the Market
+          </p>
+          <p className="px-28 text-center">
+            We are the most straightforward URL Shortener in the World. Most of the URL Shortener will track you or ask you to give your details for login. We understand your needs and hence we have created this URL Shortener
+          </p>
+          <div className='flex gap-3 justify-start items-center'>
+            <Link href="/generate"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+            <Link href="/generate"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
+          </div>
+        </div>
+        <div className="flex justify-start relative">
+          <Image className="mix-blend-darken" alt="vector" src={"/vector.jpg"} fill={true}/>
+        </div>
+      </section>
     </main>
   );
 }
--- componrnts/Navbar.js
+++ componrnts/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
 
+
 const Navbar = () => {
   return (
     <nav className='h-16 bg-purple-700 flex justify-between px-5 items-center text-white'>
-        <div className='logo font-bold text-xl'>
+        <div className='logo font-bold text-2xl'>
             BitLinks
         </div>
         <ul className='flex justify-center gap-4 items-center'>
-            <Link href="/"><li>Home</li></Link>
-            <Link href="/about"><li>About</li></Link>
-            <Link href="/generate"><li>Shorten</li></Link>
-            <Link href="/contact"><li>Contact Us</li></Link>
-            <li className='flex gap-3'>
-                <Link href="/generate"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
-                <Link href="/generate"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
+            <Link className='hover:transition-all hover:font-bold' href="/"><li>Home</li></Link>
+            <Link className='hover:transition-all hover:font-bold' href="/about"><li>About</li></Link>
+            <Link className='hover:transition-all hover:font-bold' href="/generate"><li>Shorten</li></Link>
+            <Link className='hover:transition-all hover:font-bold' href="/contact"><li>Contact Us</li></Link>
+            <li className='flex gap-3 items-center'>
+                <Link href="/generate"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+                <Link href="/generate"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
             </li>
         </ul>
     </nav>
