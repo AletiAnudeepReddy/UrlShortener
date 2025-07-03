52284daf7535d3aef035211d635c812d15dceac7
anudeep096 <aletianudeepreddy8@gmail.com>
2025-07-03 19:13:06 +0530

    ui of shortner

--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
             We are the most straightforward URL Shortener in the World. Most of the URL Shortener will track you or ask you to give your details for login. We understand your needs and hence we have created this URL Shortener
           </p>
           <div className='flex gap-3 justify-start items-center'>
-            <Link href="/generate"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
-            <Link href="/generate"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
+            <Link href="/shorten"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+            <Link href="/github"><button className='text-white hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
           </div>
         </div>
         <div className="flex justify-start relative">
--- componrnts/Navbar.js
+++ componrnts/Navbar.js
@@ -6,16 +6,16 @@ const Navbar = () => {
   return (
     <nav className='h-16 bg-purple-700 flex justify-between px-5 items-center text-white'>
         <div className='logo font-bold text-2xl'>
-            BitLinks
+            <Link className='hover:transition-all hover:font-bold' href="/">BitLinks</Link>
         </div>
         <ul className='flex justify-center gap-4 items-center'>
             <Link className='hover:transition-all hover:font-bold' href="/"><li>Home</li></Link>
             <Link className='hover:transition-all hover:font-bold' href="/about"><li>About</li></Link>
-            <Link className='hover:transition-all hover:font-bold' href="/generate"><li>Shorten</li></Link>
+            <Link className='hover:transition-all hover:font-bold' href="/shorten"><li>Shorten</li></Link>
             <Link className='hover:transition-all hover:font-bold' href="/contact"><li>Contact Us</li></Link>
             <li className='flex gap-3 items-center'>
-                <Link href="/generate"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
-                <Link href="/generate"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
+                <Link href="/shorten"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+                <Link href="/github"><button className='hover:bg-purple-400 hover:h-9 bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Github</button></Link>
             </li>
         </ul>
     </nav>
